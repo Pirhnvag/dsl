7751730f7c2a9f48192f8aa1ece8cca8eb50de22
Pirhnvag <101233860+Pirhnvag@users.noreply.github.com>
2023-03-21 22:24:06 -0300

    Update jobpoc.groovy

--- jobpoc.groovy
+++ jobpoc.groovy
@@ -1,52 +1,23 @@
-pipelineJob('job-poc') {
-  definition {
-    cps {
-      script(readFileFromWorkspace('test.jenkinsfile'))
-      sandbox()
+job('example-job') {
+    description 'This is an example job'
+    scm {
+        git('https://github.com/example/repo.git')
     }
-  }
-   parameters {
-                activeChoiceParam('DESTINO') {
-                  choiceType('SINGLE_SELECT')
-                  groovyScript {
-                    script('["seleccione:selected", "q5a", "q7a"]')
-                    fallbackScript('return ["ERROR"]')
-                description('Selecciona el Destino del proyecto. (Campo obligatorio)')
-                }
-            }
+    triggers {
+        cron('* * * * *')
+    }
+    steps {
+        shell('echo "Hello, world!"')
+    }
+    postBuild {
+        always {
+            echo 'This will always run'
         }
-  parameters {
-    stringParam('perfil_fuse', '', 'Favor ingresar perfil')
-    //choiceParam('perfil_fuse', ['q5a-persona-datos'], 'Perfil por defecto del servicio.')
-    booleanParam('cargarproperties', false, 'Indicar si desea cargar archivo de propiedades.')
-  }
-  parameters {
-    gitParam('branch') {
-    description 'Favor seleccione branch'
-    type 'BRANCH'
-    defaultValue 'testing'
+        success {
+            echo 'This will run only if the job succeeds'
+        }
+        failure {
+            echo 'This will run only if the job fails'
+        }
     }
-  }
-  definition {
-    cps {
-      script(readFileFromWorkspace('test.jenkinsfile'))
-      sandbox()
-    }
-  }
-  //parameters {
-    //activeChoiceReactiveParam('seleccione_servidor') {
-      //description('Favor seleccione servidor')
-      //filterable()
-      //choiceType('RADIO')
-      //groovyScript {
-        //script('if ( seleccione_ambiente == "Q5A") { return ["10.21.61.182", "10.21.61.183", "10.21.61.184"] } else if ( seleccione_ambiente == "Q7") { return ["10.20.73.20", "10.20.73.21", "10.20.73.22"] } ')
-        //fallbackScript('"fallback choice"')
-      //}
-      //referencedParameter('seleccione_ambiente')
-    //referencedParameter('BOOLEAN-PARAM-2')
-    //}
-  //}
-  //authorization {
-    //permission('hudson.model.Item.Build', 'q7-profile')
-  //}
 }
