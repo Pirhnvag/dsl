365424042f0e977ba1ab37053dc334a921ed71e8
Pirhnvag <101233860+Pirhnvag@users.noreply.github.com>
2023-03-22 04:10:27 -0300

    Update jobpoc.groovy

--- jobpoc.groovy
+++ jobpoc.groovy
@@ -1,23 +1,5 @@
-job('example-job') {
-    description 'This is an example job'
-    scm {
-        git('https://github.com/example/repo.git')
-    }
-    triggers {
-        cron('* * * * *')
-    }
-    steps {
-        shell('echo "Hello, world!"')
-    }
-    postBuild {
-        always {
-            echo 'This will always run'
-        }
-        success {
-            echo 'This will run only if the job succeeds'
-        }
-        failure {
-            echo 'This will run only if the job fails'
-        }
-    }
+job('lactm') {
+  steps {
+    shell('echo Hello World!')
+  }
 }
